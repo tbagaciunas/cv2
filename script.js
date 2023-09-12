ca8216fa7a7500351a0939dc2e6cb7bc7d96c8f8
tbagaciunas <113136639+tbagaciunas@users.noreply.github.com>
2023-09-12 16:09:00 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// This script will add some dynamic content to the resume
+
+var name = "Naomi Bastian Weatherford";
+var title = "Software Engineer";
+
+document.querySelector("h1").textContent = name;
+document.querySelector("h2").textContent = title;
